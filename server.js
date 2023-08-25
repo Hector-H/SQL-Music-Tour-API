8cd9553741ceeceba9c64be70719a3379fb6e162
Hector <h.hernandez12090@gmail.com>
2023-08-24 20:50:49 -0700

    turn in

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
+
+const bandsController = require('./controllers/bands_controller')
+const eventsController = require('./controllers/events_controller')
+const stagesController = require('./controllers/stages_controller')
+
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-// ROOT
+app.use('/bands', bandsController);
+app.use('/events', eventsController);
+app.use('/stages', stagesController);
+
+
 app.get('/', (req, res) => {
     res.status(200).json({
-        message: 'Welcome to the Tour API'
+        message: 'welcome to the tour API'
     })
 })
+
 
 // LISTEN
 app.listen(process.env.PORT, () => {
